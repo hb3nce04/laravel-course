b7b08feb12cebb955678e6ecb7a05ca3411cca40
Bence Hagymási <60313402+hb3nce04@users.noreply.github.com>
2024-12-17 20:56:53 +0100

    Initial commit with layouts and components

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,10 @@
+@props(['color', 'bgColor' => 'white'])
+<div {{$attributes->class("card card-text-$color card-bg-$bgColor")}}>
+    <div {{$title->attributes->class("card-header")}}>
+        {{$title}}
+    </div>
+    {{$slot}}
+    <div class="card-footer">
+        {{$footer}}
+    </div>
+</div>
