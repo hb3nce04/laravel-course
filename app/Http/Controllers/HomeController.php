f600a4e4d575d4e5de524a91e2bf093d03c9dc94
Bence Hagymási <60313402+hb3nce04@users.noreply.github.com>
2024-12-20 15:10:56 +0100

    Factories created

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Car;
 use App\Models\CarImage;
+use App\Models\Maker;
+use App\Models\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 
 class HomeController extends Controller
 {
     public function index() {
+        $model = Model::factory()->count(5)->for(Maker::factory()->state(['name'=>'Lexus']));
+        dd($model);
         return view('home.index');
     }
 }
